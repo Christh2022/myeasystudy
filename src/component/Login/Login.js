cd2036fd5b56b5f28a2d782c3fa58e023e93967d
Roldi christh MAMPASSI MOUKIETOU <christhmoukietou@gmail.com>
2023-03-27 18:55:53 +0200

    change btn color

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -3,12 +3,12 @@ import './login.css'
 
 const Login = () => {
     return (
-        <div class="login">
+        <div className="login">
             <h1>Se connecter</h1>
             <form method="post">
                 <input type="text" name="u" placeholder="Email" required="required" />
                 <input type="password" name="p" placeholder="Mot de passe" required="required" />
-                <button type="submit" class="btn btn-primary btn-block btn-large">connexion</button>
+                <button type="submit" className="btn btn-primary btn-block btn-large">connexion</button>
             </form>
         </div>
     );
