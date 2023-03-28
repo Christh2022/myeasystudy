db304a833e2c4628d408112810eb02c3cc930975
Roldi christh MAMPASSI MOUKIETOU <christhmoukietou@gmail.com>
2023-03-28 06:22:47 +0200

    payement add

--- src/component/paidComponent/Paid.js
+++ src/component/paidComponent/Paid.js
@@ -1,54 +1,33 @@
-import { signOut } from 'firebase/auth';
+//import { signOut } from 'firebase/auth';
+import axios from 'axios';
 import React, { useState } from 'react';
-import { auth } from '../../firebase';
+//import { auth } from '../../firebase';
 
 function Paid() {
-  const [response, setResponse] = useState('');
+    const [purchaseToken, setPurchaseToken] = useState(null);
 
-  const product = {
-    label: "Achat d'un Iphone S",
-    amount: 15,
-    details: "Iphone S, 32 GB, Gris...",
-  };
-  const customer = {
-    uuid: "d34f747e-8876-428c-9466-becf59177bde",
-    name: "fraise",
-    phone: "064021704",
-  };
-  const url = "https://ekolopay.com/api/v1/gateway/purchase-token?api_client=easystudy";
+  const createPayment = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/create-payment');
+      const { data } = response.data;
+      setPurchaseToken(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-  const makePurchaseRequest = () => {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        customer: JSON.stringify(customer),
-        product: JSON.stringify(product),
-        amount: 15,
-        secret_key: "a96d3a19-6e5c-46a6-9af2-fe99815b92a5"
-      }),
-      mode: 'no-cors' // option pour désactiver la sécurité CORS
-    };
-
-    fetch(url, requestOptions)
-    .then(response => response.json())
-    .then(data => {
-      if(data.response) {
-        setResponse(data.response);
-      }
-    })
-      .catch((error )=> {
-        console.log(error)
-        });
+  const redirectToPayment = () => {
+    window.location.href = `http://localhost:5000/pay/${purchaseToken}`;
   }
 
   return (
     <div>
-      <button onClick={makePurchaseRequest}>Effectuer l'achat</button>
-      {response && <p>Réponse de la requête: {response}</p>}
-      <span onClick={()=>signOut(auth)}>logout</span>
+      <button onClick={createPayment}>Créer un paiement</button>
+      {purchaseToken && <button onClick={redirectToPayment}>Payer</button>}
     </div>
   );
 }
+
 
 export default Paid;
