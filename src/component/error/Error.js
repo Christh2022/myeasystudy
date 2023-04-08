549a5271d693f0c1a6529ea45f0bb167859a61a0
Roldi christh MAMPASSI MOUKIETOU <christhmoukietou@gmail.com>
2023-04-08 21:33:45 +0200

    updates

--- src/component/error/Error.js
+++ src/component/error/Error.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Error = () => {
@@ -6,6 +7,23 @@ const Error = () => {
     const handleGoBack = ()=>{
         navigate('/login')
     }
+    const [status, setStatus] = useState()
+
+    useEffect(() => {
+        if(status !== 0){
+           console.log(status);
+           axios.post('http://localhost:5000/callback', 
+           )
+           .then(response => {
+           //setMaVariable(purchaseToken);
+                setStatus(response)
+                console.log(response);
+           })
+           .catch(error => {
+             console.log(error);
+           });
+        }
+       }, [status]);
     return (
         <div onClick={handleGoBack} style={{color: 'white', padding: '5rem', cursor: 'pointer'}}>
             connecte-toi
