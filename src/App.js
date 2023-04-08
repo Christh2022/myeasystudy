c96e8a0593a348748937072ede25cb7318449c18
Roldi christh MAMPASSI MOUKIETOU <christhmoukietou@gmail.com>
2023-04-08 21:47:57 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Chat from './component/Chatcomponent/Chat';
-import Error from './component/error/Error';
+// import Error from './component/error/Error';
 import Login from './component/Login/Login';
 import Paid from './component/paidComponent/Paid';
 import SignUp from './component/Signup/SignUp';
@@ -15,7 +15,7 @@ function App() {
         <Route path='/chat' element={<Chat purchaseToken={purchaseToken} setPurchaseToken={setPurchaseToken}/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<SignUp />}/>
-        <Route path='/*' element={<Error />}/>
+        {/* <Route path='/*' element={<Error />}/> */}
         <Route path='/' element={<Paid purchaseToken={purchaseToken} setPurchaseToken={setPurchaseToken}/>}/>
       </Routes>
     </BrowserRouter>
