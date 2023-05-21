c2188f06c63d8a9dde1374e42c0781f8069d31eb
Roldi christh MAMPASSI MOUKIETOU <christhmoukietou@gmail.com>
2023-05-21 20:27:10 +0200

    updates

--- src/component/exercise/exerciseComponent/Message.js
+++ src/component/exercise/exerciseComponent/Message.js
@@ -7,34 +7,35 @@ const Message = ({message, image}) => {
     const ref = useRef();
     const [showDate, setShowDate] = useState(false)
     const handleDate = (a , b)=>{
-        const milliseconds = a * 1000 + Math.round(b / 1000000);
-        const date = new Date(milliseconds);
-        const formattedDate = date.toLocaleString('fr-FR'); // formater la date comme une chaîne de caractères
-        return formattedDate;
-    }
-
-    useEffect(()=>{
-        if(window.innerWidth > 800){
+        if (window.innerWidth > 800) {
             setShowDate(true)
-        } else {
-            setShowDate(false)
+            const milliseconds = a * 1000 + Math.round(b / 1000000);
+            const date = new Date(milliseconds);
+            const formattedDate = date.toLocaleString('fr-FR'); // formater la date comme une chaîne de caractères
+            return formattedDate ;
         }
-    }, [setShowDate])
+    }
+    
 
     useEffect(()=>{
         ref.user?.scrollIntoView({behavior: "smooth"});
+        window.innerWidth < 800 && setShowDate(false)
     }, [message])
 
     
 
     return (
         <div ref={ref} className={`message ${message.senderId === user.uid && 'owner' }`}>
-            <div className='messageInfo'>
+            {message.text && <div className='messageInfo'>
                 <img src={message.senderId === user.uid? user.photoURL : image} alt="/" />
                 {showDate && <span>{handleDate(message.date.seconds, message.date.nanoseconds)}</span>}
-            </div>
+            </div>}
+            {message.img && <div className='messageInfo'>
+                <img src={message.senderId === user.uid? user.photoURL : image} alt="/" />
+                {showDate && <span>{handleDate(message.date.seconds, message.date.nanoseconds)}</span>}
+            </div>}
             <div className='messageContent'>
-                <p>{message.text}</p>
+                {message.text && <p>{message.text}</p>}
                 {message.img && <a href={message.img}><img src={message.img} alt="/" /></a>}
             </div>
         </div>
